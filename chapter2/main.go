e6a118f8c860ba4cbaeeb680c6e374faa13a38f3
Mevlana Ayas <mevlanaayas@gmail.com>
2018-11-14 15:23:41 +0300

    nothing important, learning types

---
+++ chapter2/main.go
@@ -0,0 +1,32 @@
+
+package main
+
+import "fmt"
+
+func main() {
+	// integer addition
+	fmt.Println("1 + 1 =", 1 + 1)
+
+	//floating number addition
+	fmt.Println("1 + 1 =", 1.0 + 1.0)
+
+	// string ops
+	fmt.Println(len("Hello, World"))
+	fmt.Println("Hello, World"[1])
+	fmt.Println("Hello, " + "World")
+
+	// George Boole ops
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+	fmt.Println("Multiplication of 32,132 and 42,452 =", 32.132 * 42.452)
+
+}
+
+/*
+answers:
+
+*/
