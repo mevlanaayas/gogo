c9da3557e924859d233c6015274292741f277d5f
Mevlana Ayas <mevlanaayas@gmail.com>
2018-11-14 14:00:54 +0300

    hello world v1

---
+++ chapter1/main.go
@@ -0,0 +1,24 @@
+
+package main
+
+import "fmt"
+
+/*
+function without args and return type
+has only one statement
+*/
+func main() {
+	fmt.Println("Hello, World")
+}
+
+/*
+run:
+	go run chapter1/main.go (linux) or chapter1\main.go (windows)
+    to see output of program. in our case it will be Hello, World in terminal
+
+build:
+    go build chapter1/main.go (linux) or chapter1\main.go (windows)
+    to create executable.
+ */
+
+ 
