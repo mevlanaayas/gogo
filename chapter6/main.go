cd0da4da2e2287483609dcf6ffb2bf2ef7184ab7
Mevlana Ayas <mevlanaayas@gmail.com>
2018-11-15 17:14:41 +0300

    finished chapter6

--- chapter6/main.go
+++ chapter6/main.go
@@ -109,6 +109,23 @@ func main()  {
 	fmt.Println("-new builtin-")
 	newEx()
 
+	fmt.Println("-Answers for questions-")
+	result, isEven := answerTwo(8)
+	fmt.Println("True and 4 ", isEven, result)
+	result, isEven = answerTwo(9)
+	fmt.Println("False and 0", isEven, result)
+
+	fmt.Println("Greatest of 12, 12, 33, 50, 0 : ", answerThree(12, 12, 33, 50, 0))
+
+	fmt.Println("Fibonacci value for 10 is : ", answerFiveFibonacci(10))
+
+	fmt.Print("Answer for sixth question: ")
+	answerSix()
+
+	fmt.Println("Confirm the answer is 1.5*1.5 : ", confirmAnswerTen())
+
+	answerEleven()
+
 }
 
 func average(scores []float64) float64  {
@@ -173,6 +190,7 @@ func second()  {
 	fmt.Println("2nd")
 
 }
+
 func deferTry()  {
 	defer second()
 	first()
@@ -233,3 +251,115 @@ func newEx()  {
 	fmt.Println("We are expecting to see value ", *xPtr)
 
 }
+
+func answerTwo(number int) (result int, isEven bool)  {
+	if number % 2 == 0 {
+		result = number / 2
+		isEven = true
+	} else {
+		result = 0
+		isEven = false
+	}
+	return
+
+}
+
+func answerThree(numbers ...int) int {
+	// we assume all input numbers are greater than zero
+	greatest := 0
+	for _, value := range numbers {
+		if value >= greatest {
+			greatest = value
+		}
+	}
+	return greatest
+
+}
+
+func answerFiveFibonacci(number int) int {
+	if number == 0 {
+		return 0
+	}
+	if number == 1 {
+		return 1
+	}
+	return answerFiveFibonacci(number - 1) + answerFiveFibonacci(number - 2)
+
+}
+
+func answerSix()  {
+	defer func() {
+		panicMessage := recover()
+		fmt.Println("Panic message from recover: ", panicMessage)
+	}()
+	panic("Answer is correct")
+
+}
+
+func confirmAnswerTen() float64 {
+	x := 1.5
+	square(&x)
+	return x
+}
+
+func square(number *float64)  {
+	*number = *number * *number
+
+}
+
+func answerEleven()  {
+	x, y := 1, 2
+	swap(&x, &y)
+	fmt.Println("X and Y is after Question 11: ", x, y)
+
+}
+
+func swap(numberOne *int, numberTwo *int)  {
+	temp := new(int)
+	*temp = *numberOne
+	*numberOne = *numberTwo
+	*numberTwo = *temp
+
+}
+
+/*
+answers:
+1-)
+	func sum(args ...int) sum int {}
+
+2-)
+	Answer is above
+
+3-)
+	Answer is above
+
+4-)
+	Ez
+
+5-)
+	Answer is above
+
+6-)
+	defer is the last call when function is over
+	panic is to throw runtime error
+	recover is to recover panics
+	Answer is above
+
+7-)
+	using & operator
+
+8-)
+	We can only assign pointer to pointer
+
+9-)
+	using "new()" builtin function
+
+10-)
+	inside square() function we have address of x (which has value of 1.5)
+	*x points to x so 1.5
+	result will be 1.5 multiply by 1.5
+
+11-)
+	Answer is above
+*/
+
