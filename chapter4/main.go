827f4d8c20abca85554005a995fd937c7e7dadaf
Mevlana Ayas <mevlanaayas@gmail.com>
2018-11-15 09:50:31 +0300

    if statements

--- chapter4/main.go
+++ chapter4/main.go
@@ -22,4 +22,23 @@ func main()  {
 	// be careful i has hold its final value
 	fmt.Println("outside of loop", i)
 
+	// if else statements
+	for i := 1; i <= 5; i++ {
+		if i % 2 == 0 {
+			fmt.Println("even")
+		} else {
+			fmt.Println("odd")
+		}
+	}
+
+	// if else if statements
+	for i := 1; i <= 10; i++ {
+		if i % 2 == 0 {
+			fmt.Println("divisible by two")
+		}else if i % 3 == 0 {
+			fmt.Println("divisible by three")
+		}else if i % 4 == 0 {
+			fmt.Println("divisible by four")
+		}
+	}
 }
