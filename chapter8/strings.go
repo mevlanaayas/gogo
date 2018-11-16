cb86cc2951ee3852092f5873a91f31e2ae7b9c60
Mevlana Ayas <mevlanaayas@gmail.com>
2018-11-16 11:14:22 +0300

    string functions

---
+++ chapter8/strings.go
@@ -0,0 +1,44 @@
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main()  {
+	// run "godoc strings -funcName- "
+	fmt.Println(strings.Contains("test", "es"))
+
+	fmt.Println(strings.Count("test", "t"))
+
+	fmt.Println(strings.HasPrefix("test", "te"))
+
+	fmt.Println(strings.HasSuffix("test", "st"))
+
+	fmt.Println(strings.Index("test", "s"))
+
+	fmt.Println(strings.Join([]string{"a", "b"}, "-"))
+
+	fmt.Println(strings.Repeat("a", 5))
+
+	fmt.Println(strings.Replace("aaaaaa", "a", "b", 2))
+
+	fmt.Println(strings.Replace("aaaaaa", "a", "b", -1))
+
+	fmt.Println(strings.Split("a-b-c-d-e", "-"))
+
+	fmt.Println(strings.ToLower("MEvlaNa"))
+
+	fmt.Println(strings.ToUpper("mevLana aYAs"))
+
+	//convert string to binary
+	arr := []byte("test")
+	// convert binary to string
+	str := string([]byte{'t', 'e', 's', 't'})
+	fmt.Println("Binary: ", arr)
+	fmt.Println("String: ", str)
+
+
+
+}
